c74f6174bcc52472e5d602a00f73d139c075b9d7
Atul Thakre <atulthakre00@gmail.com>
2021-01-01 11:07:31 +0530

    Procfile and runtime.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hacknagpur.wsgi --log-file -
--- hacknagpur/settings.py
+++ hacknagpur/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bnji%usnnhn9wl!_1i^wq&q9ue)m5$17)#g7+=z$s0fc5=tx2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psychaware.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
